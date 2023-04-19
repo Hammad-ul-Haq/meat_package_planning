2c13b2adf584dd2a8513e42971fa924ebaf7792e
Hammad-ul-Haq <hammadqureshi62@gmail.com>
2023-04-20 01:23:04 +0500

    added railway deployment settings

--- dashboard/pyodbc_sql.py
+++
@@ -1,7 +0,0 @@
-import pyodbc as odbc
-cnxn_str = ("Driver={ODBC Driver 17 for SQL Server};"
-            "Server=.;"
-            "Database=;"
-            "UID=;"
-            "PWD=;"
-            "Trusted_Connection=yes;")
