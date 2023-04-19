a12ac84c5250ca41237ad963167f48133078df8f
Hammad-ul-Haq <hammadqureshi62@gmail.com>
2023-04-20 02:00:45 +0500

    added missing dj-database package exit()

--- package_planning/settings.py
+++ package_planning/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATABASE_URL ='postgresql://postgres:K0puSzbQ2dEwKlUaBcl3@containers-us-west-37.railway.app:5645/railway'
